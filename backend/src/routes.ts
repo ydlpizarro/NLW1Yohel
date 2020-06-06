1dfbae8d54aab709d1c6ee74f444a4257da5b7cd
Yohel D. Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2020-06-06 11:02:08 -0300

    criando api

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,44 @@
 import express from 'express';
+import knex from './database/connections';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+routes.get('/items', async(request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item =>{
+        return {
+            title:item.title,
+            item_url:`http://localhost:3333/uploads/${item.image}`,
+        };
+    })
+
+    return response.json(serializedItems);
+    
+});
+
+routes.post('/points', async(request,response)=>{
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+    await knex('points').insert({
+        image:'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+    return response.json({sucess:true});
 });
 
 export default routes;
