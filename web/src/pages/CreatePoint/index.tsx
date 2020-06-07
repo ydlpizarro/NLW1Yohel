f3959717c2791d4cb06204f020d5ee3979f944d3
Yohel D. Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2020-06-07 19:25:39 -0300

    adicionando api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import { FiArrowLeft} from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker} from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
 const CreatePoint = () =>{
+    useEffect(() => {
+        api.get('items').then(response =>{
+            console.log(response);
+        });
+    },[]);
     return (
         <div id="page-create-point">
             <header>
@@ -22,18 +29,93 @@ const CreatePoint = () =>{
                     </legend>
                     <div className="field">
                         <label htmlFor="name">Nome da entidade</label>
+                        <input 
+                            type="text"
+                            name="name"
+                            id="name"
+                        />
                     </div>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input 
+                                type="email"
+                               name="email"
+                               id="email"
+                            />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input 
+                                type="text"
+                               name="whatsapp"
+                               id="whatsapp"
+                            />
+                        </div>
+                    </div>                   
                 </fieldset>
                 <fieldset>
                     <legend>
                         <h2>Endereço</h2>
+                        <span>Seleccione o endereço no mapa</span>
                     </legend>
+                    <Map center={[-22.8826005,-48.4287712]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-22.8826005,-48.4287712]}/>
+                    </Map>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Seleccione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Seleccione uma Cidade</option>
+                            </select>
+                        </div>
+                    </div>
                 </fieldset>
                 <fieldset>
                     <legend>
                         <h2>Ítens de coleta</h2>
+                        <span>Seleccione um ou mais ítens abaixo</span>
                     </legend>
+                    <ul className="items-grid">
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
+                            <span>Lâmpadas</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
+                            <span>Lâmpadas</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
+                            <span>Lâmpadas</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
+                            <span>Lâmpadas</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
+                            <span>Lâmpadas</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
+                            <span>Lâmpadas</span>
+                        </li>
+                    </ul>
                 </fieldset>
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
             </form>
         </div>
     );
